7c61249f998ff75b91247d6be7b0509a7b8b1f39
anise <anisebamford@gmail.com>
2023-02-21 14:53:16 -0800

    remove debug function

--- components/DarkModeToggle.tsx
+++ components/DarkModeToggle.tsx
@@ -4,8 +4,5 @@ import {LocalContext} from "../context/LocalContext"
 export function DarkModeToggle() {
   const {darkMode, toggleDarkMode} = useContext(LocalContext)
 
-  const fn = () => {
-    toggleDarkMode()
-  }
   return <button onClick={toggleDarkMode}> {darkMode ? "D" : "L"} </button>
 }
