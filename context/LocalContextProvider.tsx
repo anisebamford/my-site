d26b3c2ee99bdf87e4f5f00e6042db6a1e2b8f89
Charlie Bamford <charliejoybamford@gmail.com>
2023-07-25 13:30:55 -0700

    Fix requisitioning window

--- context/LocalContextProvider.tsx
+++ context/LocalContextProvider.tsx
@@ -4,33 +4,26 @@ import {getStateFromStorage, setStateToStorage} from "./storage"
 
 const STATE_KEY = "LOCAL_CONTEXT_STATE";
 
-function getStateFromStorage() {
-  const valueInStorage = window?.localStorage.getItem(STATE_KEY);
-  return valueInStorage ? JSON.parse(valueInStorage) as TLocalContextState : null;
-}
-
-function setStateToStorage(state) {
-  window?.localStorage.setItem(STATE_KEY, JSON.stringify(state));
-}
-
 export function LocalContextProvider({children}: PropsWithChildren) {
   const [contextState, setContextState] = useState({darkMode: false})
 
+  const myWindow: Window = typeof window !== "undefined" ? window : null;
+
   const setContextStateToMemoryAndStorage = useCallback(function setContextStateToMemoryAndStorage(newState) {
-    setStateToStorage(newState)
+    setStateToStorage(newState, myWindow)
     setContextState(newState)
   }, [setContextState])
 
   useEffect(() => {
-    const localState = getStateFromStorage()
+    const localState = getStateFromStorage(myWindow)
     if (!localState) {
-      setStateToStorage(contextState)
+      setStateToStorage(contextState, myWindow)
     }
     else if(JSON.stringify(contextState) !== JSON.stringify(localState)) {
       setContextState(localState)
     }
   }, [contextState, setContextState])
-  
+
   const toggleDarkMode = useCallback(function toggleDarkMode() {
     const newContextState = {...contextState}
     newContextState.darkMode = !contextState.darkMode
